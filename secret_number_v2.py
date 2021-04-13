238e722df1ff10cacb1718293e4257228f524ae7
DavidWeiner <44605259+DavidWeiner@users.noreply.github.com>
2021-04-13 12:36:35 +0200

    nem tokeletes meg

--- secret_number_v2.py
+++ secret_number_v2.py
@@ -3,6 +3,7 @@ n = random.randint(1 , 5)
 guess = int(input("Enter your number, between 1-5\n"))
 #print(n)
 #miert nem irja ki amikor kitalaltam, ezt meg ki kell nyomozni?
+#ciklusnak utana kell nezni
 while n != guess:
     if n == guess:
         print("you guessed it!")
