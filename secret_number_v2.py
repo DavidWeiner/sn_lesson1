3b692459b2e2bbf3633443ffdb6d3ce141e1b2cc
DavidWeiner <44605259+DavidWeiner@users.noreply.github.com>
2021-04-13 12:26:04 +0200

    calculator comment

--- secret_number_v2.py
+++ secret_number_v2.py
@@ -2,7 +2,7 @@ import random
 n = random.randint(1 , 5)
 guess = int(input("Enter your number, between 1-5\n"))
 #print(n)
-#miert nem irja ki amikor kitalaltam?
+#miert nem irja ki amikor kitalaltam, ezt meg ki kell nyomozni?
 while n != guess:
     if n == guess:
         print("you guessed it!")
