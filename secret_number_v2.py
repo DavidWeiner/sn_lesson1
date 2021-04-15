6bab7b3a250ce06e278ae222ef7dbcba3ee7fed4
DavidWeiner <44605259+DavidWeiner@users.noreply.github.com>
2021-04-15 10:54:23 +0200

    felesleges kommentek torolve

--- secret_number_v2.py
+++ secret_number_v2.py
@@ -1,10 +1,8 @@
 import random
 n = random.randint(1 , 5)
 guess = 0
-#guess = int(input("Enter your number, between 1-5\n"))
+
 #print(n)
-#miert nem irja ki amikor kitalaltam, ezt meg ki kell nyomozni?
-#ciklusnak utana kell nezni
 while n != guess:
     guess = int(input("Enter your number, between 1-5\n"))
     if n == guess:
