7891224bed9d0566b350f3c6c518bffd1083db4b
DavidWeiner <44605259+DavidWeiner@users.noreply.github.com>
2021-04-15 10:14:12 +0200

    while loop javitva

--- secret_number_v2.py
+++ secret_number_v2.py
@@ -1,12 +1,13 @@
 import random
 n = random.randint(1 , 5)
-guess = int(input("Enter your number, between 1-5\n"))
+guess = 0
+#guess = int(input("Enter your number, between 1-5\n"))
 #print(n)
 #miert nem irja ki amikor kitalaltam, ezt meg ki kell nyomozni?
 #ciklusnak utana kell nezni
 while n != guess:
+    guess = int(input("Enter your number, between 1-5\n"))
     if n == guess:
-        print("you guessed it!")
+        print(f"you guessed it! the correct number is {n}")
     else:
         print("wrong number")
-        guess = int(input("Enter your number, between 1-5\n"))
