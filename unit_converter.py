4fc6e8a2798d61e55ccd71f50528dd9761cdd5d5
DavidWeiner <44605259+DavidWeiner@users.noreply.github.com>
2021-04-16 16:42:34 +0200

    kerdes kommentben

--- unit_converter.py
+++ unit_converter.py
@@ -6,7 +6,7 @@ while True:
     miles = km * 0.621371
     print(f"{km}km is equal to {miles}mile(s)")
     answer = input("would you like to do another convert? please type y/n \n")
-
+# hogy lehet levagni a tizedes pontot, mondjuk .2 tizedesjegyig
     if answer != "y":
         print("ok, bye")
         break
