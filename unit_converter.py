f397a7a16502ea2450b5e3001391609b77be2836
DavidWeiner <44605259+DavidWeiner@users.noreply.github.com>
2021-04-16 16:34:20 +0200

    km to miles converter

---
+++ unit_converter.py
@@ -0,0 +1,12 @@
+print("this program convert km to miles")
+repeat = "n"
+
+while True:
+    km = int(input("please enter the km\n"))
+    miles = km * 0.621371
+    print(f"{km}km is equal to {miles}mile(s)")
+    answer = input("would you like to do another convert? please type y/n \n")
+
+    if answer != "y":
+        print("ok, bye")
+        break
